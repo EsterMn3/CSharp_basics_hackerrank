d1ec792412c74506da3741e077f754e3cdf55c02
EsterMn3 <145455804+EsterMn3@users.noreply.github.com>
2024-04-08 00:32:45 +0200

    Add files via upload

---
+++ EmployeesManagement/Program.cs
@@ -0,0 +1,128 @@
+﻿/*3. OldestAgeForEachCompany - finds the oldest aged employee for each unique company and returns the results as a Dictionary<string, Employee> sorted by key, where Key[string] is the unique company name, and Value[Employee] is the oldest employee in this company.
+2
+Here is the description of the Employee class:
+* FirstName [string] - the first name of the emplovee.
+LastName[string] - the last name of the employee.
+* Company [string] - the company of the emplovee.
+* Age [int] - the age of the employee.*/
+
+
+// Importing namespaces that contain classes and methods used in this code file.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+// Defining the namespace for the current solution.
+namespace Solution
+{
+    public class Solution
+    {
+        // Defining a method to calculate the average age for each company.
+        public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+        {
+            // Creating a new dictionary to store the results.
+            var result = new Dictionary<string, int>();
+
+            // Iterating over each unique company name in the list of employees.
+            foreach (var company in employees.Select(x => x.Company).Distinct().OrderBy(x => x))
+            {
+                // Calculating the average age of employees for the current company and rounding it to the nearest whole number.
+                result.Add(company, (int)Math.Round(employees.Where(x => x.Company == company).Average(y => y.Age), 0));
+            }
+
+            // Returning the dictionary containing the average age for each company.
+            return result;
+        }
+
+        // Defining a method to count the number of employees for each company.
+        public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+        {
+            // Creating a new dictionary to store the results.
+            var result = new Dictionary<string, int>();
+
+            // Iterating over each unique company name in the list of employees.
+            foreach (var company in employees.Select(x => x.Company).Distinct().OrderBy(x => x))
+            {
+                // Counting the number of employees for the current company.
+                result.Add(company, employees.Count(x => x.Company == company));
+            }
+
+            // Returning the dictionary containing the count of employees for each company.
+            return result;
+        }
+
+        // Defining a method to find the oldest employee for each company.
+        public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+        {
+            // Creating a new dictionary to store the results.
+            var result = new Dictionary<string, Employee>();
+
+            // Iterating over each unique company name in the list of employees.
+            foreach (var company in employees.Select(x => x.Company).Distinct().OrderBy(x => x))
+            {
+                // Finding the oldest employee for the current company.
+                result.Add(company, employees.Where(x => x.Company == company).OrderByDescending(y => y.Age).First());
+            }
+
+            // Returning the dictionary containing the oldest employee for each company.
+            return result;
+        }
+
+        // Defining the main method, which serves as the entry point of the program.
+        public static void Main()
+        {
+            // Reading the number of employees from the console input and parsing it to an integer.
+            int countOfEmployees = int.Parse(Console.ReadLine());
+
+            // Creating a new list to store the employee data.
+            var employees = new List<Employee>();
+
+            // Looping over the input data to read employee details and populate the list.
+            for (int i = 0; i < countOfEmployees; i++)
+            {
+                // Reading a line of input from the console.
+                string str = Console.ReadLine();
+                
+                // Splitting the input line into an array of strings based on space (' ') delimiter.
+                string[] strArr = str.Split(' ');
+
+                // Creating a new Employee object and adding it to the list.
+                employees.Add(new Employee
+                {
+                    FirstName = strArr[0],   // Assigning the first name of the employee.
+                    LastName = strArr[1],    // Assigning the last name of the employee.
+                    Company = strArr[2],     // Assigning the company of the employee.
+                    Age = int.Parse(strArr[3])  // Parsing and assigning the age of the employee.
+                });
+            }
+
+            // Printing the average age for each company.
+            foreach (var emp in AverageAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+            }
+
+            // Printing the count of employees for each company.
+            foreach (var emp in CountOfEmployeesForEachCompany(employees))
+            {
+                Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+            }
+
+            // Printing the oldest employee for each company.
+            foreach (var emp in OldestAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+            }
+        }
+    }
+
+    // Defining a class named Employee to represent employee data.
+    public class Employee
+    {
+        public string FirstName { get; set; }  // Property to store the first name of the employee.
+        public string LastName { get; set; }   // Property to store the last name of the employee.
+        public int Age { get; set; }           // Property to store the age of the employee.
+        public string Company { get; set; }    // Property to store the company of the employee.
+    }
+}
---
+++ NotesStore/Program.cs
@@ -0,0 +1,114 @@
+﻿/*In this challenge, the task is to implement a class called NotesStore. The class will manage a collection of notes, with each note having a state and a name. Valid states for notes are 'completed',
+"active', and 'others'. All other states are invalid.
+The class must have the following methods:
+1. AddNote(state, namel: adds a note with the given name (string) and state (string) to the collection. In addition to that:
+﻿﻿If the passed name is empty, then it throws an Exception with the message Name cannot be empty'.
+﻿﻿If the passed name is non-empty but the given state is not a valid state for a note, then it throws an Exception with the message
+Invalid state (state)
+2. GetNotes(state) returns a list of note names with the given state (string) added so far. The names are returned in the order the corresponding notes were added. In addition to that:
+if the given state is not a valid note state, then it throws an Exception with the message Invalid state (states
+• IF no note is found in this state, it returns an empty list
+Note: The state names are case-sensitive.*/
+
+using System;
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace Solution
+{
+    public class Note
+    {
+        public string state;
+        public string name;
+    }
+
+    public class NotesStore
+    {
+        List<Note> notes;
+        
+        public NotesStore()
+        {
+            notes = new List<Note>();
+        }
+        
+        public void AddNote(string state, string name)
+        {
+            if (name.Length == 0)
+            {
+                throw new Exception("Name cannot be empty");
+            }
+            else if (!isValid(state))
+            {
+                throw new Exception($"Invalid state {state}");
+            }
+            else
+            {
+                Note note = new Note();
+                note.name = name;
+                note.state = state;
+                notes.Add(note);
+            }
+        }
+
+        public List<string> GetNotes(string state)
+        {
+            if (!isValid(state))
+            {
+                throw new Exception($"Invalid state {state}");
+            }
+
+            List<string> noteNames = notes
+                .Where(note => note.state == state)
+                .Select(note => note.name)
+                .ToList();
+
+            return noteNames;
+        }
+
+        public bool isValid(string state)
+        {
+            switch (state)
+            {
+                case "completed":
+                case "active":
+                case "others":
+                    return true;
+                default:
+                    return false;
+            
+        }
+    }
+    } 
+
+    public class Solution
+    {
+        public static void Main() 
+        {
+            var notesStoreObj = new NotesStore();
+            var n = int.Parse(Console.ReadLine());
+            for (var i = 0; i < n; i++) {
+                var operationInfo = Console.ReadLine().Split(' ');
+                try
+                {
+                    if (operationInfo[0] == "AddNote")
+                        notesStoreObj.AddNote(operationInfo[1], operationInfo.Length == 2 ? "" : operationInfo[2]);
+                    else if (operationInfo[0] == "GetNotes")
+                    {
+                        var result = notesStoreObj.GetNotes(operationInfo[1]);
+                        if (result.Count == 0)
+                            Console.WriteLine("No Notes");
+                        else
+                            Console.WriteLine(string.Join(",", result));
+                    } else {
+                        Console.WriteLine("Invalid Parameter");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+    }
+}
